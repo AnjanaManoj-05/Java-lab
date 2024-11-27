3dd1155aa2561150f11f0086633e86f0b3fdd2c5
AnjanaManoj-05 <anjanamanoj.cs23@bmsce.ac.in>
2024-11-27 14:29:13 +0530

    Update implementsthreads.java

--- week8/implementsthreads.java
+++ week8/implementsthreads.java
@@ -10,7 +10,7 @@ catch(InterruptedException e){
 }}
 }
 
-class Multithreading2 extends Thread{
+class Multithreading2 implements Runnable{
 public void run(){
 for(int i=0;i<5;i++){
   System.out.println("CSE");
